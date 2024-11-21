780d5e5fa01a2f747f31b8fc1f9c6e7c6023b78a
Dmitrijs Tolstihs <81509127+jellybuba@users.noreply.github.com>
2024-11-21 15:29:14 +0200

    Update main.py
    main update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from division import division
 from mutiplication import multiplication
+from addition import add_two_numbers
+from subtraction import sub_two_numbers
 
 
 def main():
@@ -7,5 +9,9 @@ def main():
     print(x)
     y = multiplication(5,2)
     print(y)
+    z = add_two_numbers(4,7)
+    print(z)
+    c = sub_two_numbers(9,7)
+    print(c)
 if __name__ == "__main__":
     main()
